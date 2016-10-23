59637f3bd998fba5155caf0abe5818a4a8a1d35e
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-10-23 18:32:22 +0300

    linear increase in the first 1ns

--- I_in.cc
+++ I_in.cc
@@ -5,10 +5,14 @@ void I_in(float pulseData[], float timeData[], double tDecay){
     // Simulation time step (in ns)
     const double dt = 0.5;
     
-    for (unsigned i=0; i<QIE8Simulator::maxlen; ++i)
+    pulseData[0]=0;
+    pulseData[1]=(exp(-1/tDecay)/tDecay ) / 2;  // linear increase in the first 1 ns
+    timeData[0]=0;
+    timeData[1]=dt;
+    for (unsigned i=2; i<QIE8Simulator::maxlen -2 ; ++i)
     {
         const double t = i*dt;
-        pulseData[i] = exp(-t/tDecay)/tDecay;
+        pulseData[i] = exp(-(t-1)/tDecay)/tDecay;
         timeData[i]=t;
     }
     
