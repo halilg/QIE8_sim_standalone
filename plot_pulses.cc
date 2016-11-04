7d0f18526a05531fc8b23a41b14281dec9419d1b
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-11-04 12:57:16 +0300

    fixes

--- plot_pulses.cc
+++ plot_pulses.cc
@@ -11,14 +11,17 @@ int main()
     c1->SetGrid();
     TMultiGraph *mg = new TMultiGraph();
     TGraph *gr0 = new TGraph("ref_pulse_norm.txt");
-    TGraph *gr1 = new TGraph("ref_pulse_norm.txt");
-    gr1->SetLineColor(kGray);
-    gr1->SetLineColor(kBlue);
+    TGraph *gr1 = new TGraph("pulse.txt");
+    TGraph *gr2 = new TGraph("pulse_preamp.txt");
+    gr0->SetLineColor(kGray);
+    gr1->SetLineColor(kOrange);
+    gr2->SetLineColor(kBlue);
     //gr1->SetMarkerStyle(21);
     //gr1->Fit("pol6","q");
     
     mg->Add(gr0);
     mg->Add(gr1);
+    mg->Add(gr2);
     
     mg->Draw("al");
     mg->GetXaxis()->SetTitle("t (ns)"); 
@@ -27,9 +30,9 @@ int main()
     mg->GetXaxis()->SetLimits(-5.,150.);
     
     TLegend * leg = new TLegend(0.62,0.79,0.89,0.89); //coordinates are fractions of pad dimensions
+    leg->AddEntry(gr2,"Preamp simulated pulse","l"); 
     leg->AddEntry(gr1,"Simulated pulse","l"); 
     leg->AddEntry(gr0,"Reference pulse","l"); 
-    //leg->SetHeader("HPD Pulse");
     leg->Draw();    
     
     c1->Print("pulse.png");
@@ -37,14 +40,14 @@ int main()
     
     //const double TSs[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     
-    TGraph *gr2 = new TGraph("pulse_ts.txt");
+    TGraph *gr5 = new TGraph("ADC_vs_TS.txt");
     c1->Clear();
 
-    gr2->SetTitle("");
-    gr2->GetXaxis()->SetTitle("TS"); 
-    gr2->GetYaxis()->SetTitle("Q");
-        gr2->SetFillColor(13);
-    gr2->Draw("AB");
+    gr5->SetTitle("");
+    gr5->GetXaxis()->SetTitle("TS"); 
+    gr5->GetYaxis()->SetTitle("Q");
+    gr5->SetFillColor(13);
+    gr5->Draw("AB");
     c1->Print("adc.png");
     
     
