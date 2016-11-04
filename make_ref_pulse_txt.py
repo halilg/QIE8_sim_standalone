a7fc771886700e62a7bd766b53f9090a794da920
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-11-04 17:45:11 +0300

    code accepts cc files directly

--- make_ref_pulse_txt.py
+++ make_ref_pulse_txt.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
-import sys
+import os, sys
 #   TH1F *h_1_in__3 = new TH1F("h_1_in__3","h_1_in",500,-0.125,124.875
 
-ls=file("ref_pulse.cc").readlines()
+if len(sys.argv) < 2: sys.exit()
+
+ifname=sys.argv[1]
+
+
 
 dt=(124.875+0.125)/500
 i=0
 
-integral=0
-#integrate:
-for l in ls:
-    i=i+1
-    integral=integral+dt*float(l.strip().split(",")[1])
-
+ls=file(ifname).readlines()
 
 i=0
 print 0,0
 for l in ls:
+    if not "SetBinContent" in l: continue
+    l=l.strip().split(",")[1]
+    l=l.replace(");","")
     i=i+1
-    print i*dt,float(l.strip().split(",")[1])/integral
+    print i*dt,l
     
-while i*dt < 749.5:
-    i=i+1
-    print i*dt,0
