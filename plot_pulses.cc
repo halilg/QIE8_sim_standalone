8799b89895cd476671ad53f2e407107b9ff653e0
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-11-05 13:41:17 +0300

    input files can be specified from command line; plots output in pdf

--- plot_pulses.cc
+++ plot_pulses.cc
@@ -3,23 +3,68 @@
 #include "TAxis.h"
 #include "TGraph.h"
 #include "TLegend.h"
+#include "string.h"
 
-int main()
+#include "CmdLine.hh"
+
+using namespace std;
+
+static void print_usage(const char* progname)
 {
+    cout << "\nUsage: " << progname << " <pulse> <preamp simulated pulse> <reference preamp simulated pulse>\n\n"
+         << "This program makes a comparison plot of the preamp input pulse, preamp simulated pulse and reference preamp simulated pulse\n"
+         << "Command line arguments are as follows:\n\n"
+         << "  <pulse>  normally pulse.txt\n\n"
+         << "  <preamp simulated pulse> normally pulse_preamp.txt\n\n"
+         << "  <reference preamp simulated pulse>  normally data/h_<charge>_out.txt\n"
+         << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    
+    // Parse the command line arguments
+    CmdLine cmdline(argc, argv);
+
+    if (argc == 1)
+    {
+        print_usage(cmdline.progname());
+        return 0;
+    }
+
+    string iPulse, oPulse, oPulse_ref;
+
+    try {
+        cmdline.optend();
+
+        if (cmdline.argc() != 3)
+            throw CmdLineError("wrong number of command line arguments");
+        cmdline >> iPulse >> oPulse >> oPulse_ref;
+
+        //if (tDecay <= 0.0)
+            //throw CmdLineError("Decay time argument must be positive");
+    }
+    catch (CmdLineError& e) {
+        cerr << "Error in " << cmdline.progname() << ": "
+             << e.str() << endl;
+        print_usage(cmdline.progname());
+        return 1;
+    }    
+    
     
     TCanvas *c1 = new TCanvas("c1","multigraph",700,500);
     c1->SetGrid();
     TMultiGraph *mg = new TMultiGraph();
     //TGraph *gr0 = new TGraph("ref_pulse_norm.txt");
-    TGraph *gr0 = new TGraph("h_2_out.txt");
+    TGraph *gr0 = new TGraph(oPulse_ref.c_str());
     gr0->SetLineColor(kOrange);
     gr0->SetLineWidth(2);
 
-    TGraph *gr1 = new TGraph("pulse.txt");
+    TGraph *gr1 = new TGraph(iPulse.c_str());
     gr1->SetLineColor(30);
     gr1->SetLineWidth(2);
 
-    TGraph *gr2 = new TGraph("pulse_preamp.txt");
+    TGraph *gr2 = new TGraph(oPulse.c_str());
     gr2->SetLineColor(kBlue);
     gr2->SetLineWidth(2);
     
@@ -46,7 +91,7 @@ int main()
     
     leg->Draw();    
     
-    c1->Print("pulse.png");
+    c1->Print("pulse.pdf");
     
     
     //const double TSs[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -59,7 +104,7 @@ int main()
     gr5->GetYaxis()->SetTitle("Q");
     gr5->SetFillColor(13);
     gr5->Draw("AB");
-    c1->Print("adc.png");
+    c1->Print("adc.pdf");
     
     
     return 0;
