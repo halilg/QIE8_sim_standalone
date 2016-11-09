118393c3a02d0372e9773f0f805b808734d9a3cb
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-11-09 22:55:24 +0300

    first working version

--- simulator.cc
+++ simulator.cc
@@ -1,11 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include "QIE8Simulator.h"
 #include "grapher.h"
+#include "telegrapher.h"
 #include "PulseModel.h"
+
+void dump_pulse(float pulse[], const unsigned int size, double dt, const char * fname){
+    std::ofstream myfile(fname);
+    for (unsigned int i=0;i<size; i++){
+        myfile << i*dt << " " << pulse[i] << std::endl;
+    }
+}
 
 int main(){
     const double dt = 0.5;
-    float pulseData[QIE8Simulator::maxlen];
-    memset(pulseData, 0, sizeof(pulseData)); // clear the memory
-    RefPulseModel(pulseData, dt);
+    float pulse0[QIE8Simulator::maxlen];
+    float pulse1[QIE8Simulator::maxlen];
+ 
+    memset(pulse0, 0, sizeof(pulse0)); // clear the memory
+    memset(pulse1, 0, sizeof(pulse1)); // clear the memory
+    
+    RefPulseModel(pulse0, dt);
+
+    for (unsigned int i=0; i<QIE8Simulator::maxlen;i++){
+        pulse0[i]=pulse0[i]*3.03E-5;
+    }
+    
+    hGraph gr1(dt, pulse0);
+    gr1.lineColor=kBlue;
+    gr1.lineWidth=2;
+    
+    dump_pulse(pulse0, QIE8Simulator::maxlen, dt, "test_refpulse.txt");
+
+    float Vin=100.;  // Volt
+    float R=1.0E-3; // Ohm
+    float L=0.1E-6; // Henry
+    float G=0.0; //Ohm
+    float C=10.0E-12; //Farad
+    float dz=1.0; //m
+    telegrapher(pulse0, pulse1, QIE8Simulator::maxlen, dt, Vin, R, L, G, C, dz);
+    dump_pulse(pulse1, QIE8Simulator::maxlen, dt, "test_telpulse.txt");
+    
+    hGraph gr2(dt, pulse0);
+    gr2.lineColor=kGreen;
+    gr2.lineWidth=2;
+    gr2.lineStyle=2;
+    
+    hGrapher nmg;
+    nmg.add(gr1);
+    nmg.add(gr2);
+    nmg.xAxisLimits[0]=-5;
+    nmg.xAxisLimits[1]=150;
+    nmg.xAxisTitle="t (ns)";
+    nmg.yAxisTitle="Pulse Current";
+    nmg.print("test.pdf");
+    
+    
+    
 }
--- telegrapher.cc
+++ telegrapher.cc
@@ -1,7 +1,10 @@
 // Models RLC transmission line (Telegrapher's equations)
 #include <iostream>
 
-void telegrapher(const float Iin[], float Iout[], unsigned int asize, float dt, float Vin, float R, float L, float G, float C, float dz) {
+void telegrapher(const float Iin[], float Iout[],
+                 const unsigned int asize, const float dt,
+                 const float Vin, const float R, const float L, const float G, const float C,
+                 const float dz) {
 
     float Vout, Vout_prev;
     float Iin_dot, Vout_dot;
@@ -10,12 +13,16 @@ void telegrapher(const float Iin[], float Iout[], unsigned int asize, float dt, 
     Vout=0;
     Vout_prev=0;
     
+    
+    
     for (unsigned int i=1; i<asize; i++){
         Iin_dot=(Iin[i]-Iin[i-1])/dt;
         Vout = Vin- R*dz*Iin[i] - L*dz* Iin_dot;
         Vout_dot = (Vout - Vout_prev)/dt;
         Vout_prev=Vout;
         Iout[i] = Iin[i] - G*dz*Vout - C*dz*Vout_dot;
+        //if (Iout[i]<0)Iout[i]=0;
+        std::cout << Iout[i] << " " << Iin[i] << std::endl;
     }
     
 }
