eb1423321bf44469839efba62d1608ec9f1c8b11
Halil Gamsizkan <halilgamsizkan@gmail.com>
2016-11-09 22:54:12 +0300

    adding file

---
+++ telegrapher.h
@@ -0,0 +1,5 @@
+
+void telegrapher(const float Iin[], float Iout[],
+                 const unsigned int asize, const float dt,
+                 const float Vin, const float R, const float L, const float G, const float C,
+                 const float dz);
